0e29537e802ea99c9ff6fc04a9f538d8ea1a8e3a
tpoole98 <tpoole10@uncc.edu>
2021-09-08 22:56:50 -0400

    NumInt

---
+++ NumInt/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <chrono>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  
+float f1(float x, int intensity);
+float f2(float x, int intensity);
+float f3(float x, int intensity);
+float f4(float x, int intensity);
+
+#ifdef __cplusplus
+}
+#endif
+
+  
+int main (int argc, char* argv[]) {
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+
+  int functionid;
+  std::cin >> functionid;
+  int a;
+  std::cin >> a;
+  int b;
+  std::cin >> b;
+  float n;
+  std::cin >> n;
+  int intensity;
+  std::cin >> intensity;
+  
+
+  if (argc < 6) {
+    std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity>"<<std::endl;
+    return -1;
+  }
+
+  if(functionid == 1)
+    f1(n, intensity);
+  else if(functionid == 2)
+    f2(n, intensity);
+  else if(functionid == 3)
+    f3(n, intensity);
+    else if (functionid == 4)
+      f4(n, intensity);
+
+
+    auto t2 = std::chrono::high_resolution_clock::now();
+
+
+  auto duration =std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();
+  double duration1 = duration / 1000000.0;
+  
+  std::cerr << std::to_string(duration1);
+
+  
+  return 0;
+}
