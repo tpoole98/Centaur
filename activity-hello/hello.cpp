e98a4006c24e93bd836b18e61737f6a6d0ef709e
tpoole98 <tpoole10@uncc.edu>
2021-09-08 22:07:57 -0400

    Activity-hello

--- activity-hello/hello.cpp
+++ activity-hello/hello.cpp
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
 
 int main () {
-  printf("hello\n");
+  char hostname[HOST_NAME_MAX];
+  gethostname(hostname, HOST_NAME_MAX);
+  printf("hostname: %s", hostname);
   return 0;
 }
